e9f903230622d328f3516240284e555b66390734
Nikita <nikita.rabchuk@gmail.com>
2022-01-16 18:36:20 +0300

    Сделал функцию сортировки строк

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,15 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = "asc") {
+    const newArr = [...arr];
+    const compareOptions = {
+        caseFirst: "upper",
+    }
 
+    if (param === "asc") {
+        return newArr.sort((a, b) => a.localeCompare(b, [], compareOptions));
+    } else if (param === "desc") {
+        return newArr.sort((a, b) => b.localeCompare(a, [], compareOptions));
+    }
 }
