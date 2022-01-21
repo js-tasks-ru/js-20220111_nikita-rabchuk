9356c2a3fd60354babdf8c5074444c78ea36f99f
Nikita <nikita.rabchuk@gmail.com>
2022-01-21 20:05:43 +0300

    made function invert object, made function uniq

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,41 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const closurePath = path.split(".");
 
+  return function createGetter(obj) {
+    let recCounter = 0;
+
+    const getNextValue = (prevValue, counter) => {
+      const currentProp = closurePath[counter];
+      const nextProp = closurePath[counter++];
+
+      if (prevValue[nextProp]) {
+        return getNextValue(prevValue[nextProp], counter++);
+      } else {
+        if (prevValue[currentProp]) {
+          return prevValue[currentProp];
+        }
+        return prevValue;
+      }
+
+      // if (prevValue[currentProp]) {
+      //   return prevValue[currentProp];
+      // }
+
+      // return prevValue;
+    };
+
+    return getNextValue(obj, recCounter);
+  };
 }
+
+// const product = {
+//   category: {
+//     title: 'asd',
+//   },
+// };
+
+// const getter = createGetter("category.title.name");
+
+// console.log(getter(product));
