3e8dc4cc2ebbbcd1172759b56376a5f39f397b9f
Nikita <nikita.rabchuk@gmail.com>
2022-01-30 23:58:09 +0300

    created a Notification component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,66 @@
 export default class NotificationMessage {
+  element;
+  timerId;
 
+  static prevNotification;
+
+  constructor(message = "", { duration = 1000, type = "success" } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+        <div class="notification ${this.type}" style="--value:${
+      this.duration / 1000
+    }s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+            <div class="notification-header">Notification</div>
+            <div class="notification-body">
+                ${this.message}
+            </div>
+            </div>
+        </div>
+  `;
+  }
+
+  render() {
+    const wrap = document.createElement("div");
+
+    wrap.innerHTML = this.getTemplate();
+
+    this.element = wrap.firstElementChild;
+  }
+
+  show(parent = document.body) {
+    if (NotificationMessage.prevNotification) {
+      NotificationMessage.prevNotification.remove();
+    }
+
+    parent.append(this.element);
+
+    this.timerId = setTimeout(() => this.remove(), this.duration);
+
+    NotificationMessage.prevNotification = this;
+  }
+
+  remove() {
+    clearTimeout(this.timerId);
+
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+      this.remove();
+
+      this.element = null;
+
+      NotificationMessage.prevNotification = null;
+  }
 }
